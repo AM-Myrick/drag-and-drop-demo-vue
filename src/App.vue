7a922b8f720e993fed0476da8c9de41545d35dfc
AM-Myrick <amyrick.820@gmail.com>
2020-03-08 23:09:57 -0400

    commit finished project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,157 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="app"
+    :class="{ 'starry-night': isStarryNight, 'yin-yang': isYinYang, 'polka-dot': isPolkaDot, 'cicada-stripes': isCicadaStripes }"
+  >
+    <draggable class="draggable" v-model="icons" group="icons" @start="drag=true" @end="drag=false">
+      <IconWrapper
+        v-for="(icon, index) of icons"
+        :key="index"
+        :icon="icon"
+        :isHighResScreen="isHighResScreen"
+      />
+    </draggable>
+    <div class="draggable">
+      <div
+        class="icon-wrapper background-selector"
+        v-for="(selector, index) of selectors"
+        @click="setBackground"
+        :key="index"
+      >
+        <div class="icon-styles" :class="selector.className"></div>
+        <p>Select Background {{selector.pattern}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IconWrapper from "./components/IconWrapper";
+import draggable from "vuedraggable";
+import "./index.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    IconWrapper,
+    draggable
+  },
+  data() {
+    return {
+      isStarryNight: true,
+      isYinYang: false,
+      isPolkaDot: false,
+      isCicadaStripes: false,
+      selectors: [
+        {
+          pattern: "Starry Night",
+          className: "starry-night"
+        },
+        {
+          pattern: "Yin Yang",
+          className: "yin-yang"
+        },
+        {
+          pattern: "Polka Dot",
+          className: "polka-dot"
+        },
+        {
+          pattern: "Cicada Stripes",
+          className: "cicada-stripes"
+        }
+      ],
+      isHighResScreen: window.devicePixelRatio > 1,
+      icons: [
+        {
+          label: "label 1",
+          iconName: "001"
+        },
+        {
+          label: "label 2",
+          iconName: "002"
+        },
+        {
+          label: "label 3",
+          iconName: "003"
+        },
+        {
+          label: "label 4",
+          iconName: "004"
+        },
+        {
+          label: "label 5",
+          iconName: "005"
+        },
+        {
+          label: "label 6",
+          iconName: "006"
+        },
+        {
+          label: "label 7",
+          iconName: "007"
+        },
+        {
+          label: "label 8",
+          iconName: "008"
+        },
+        {
+          label: "label 9",
+          iconName: "009"
+        },
+        {
+          label: "label 10",
+          iconName: "010"
+        },
+        {
+          label: "label 11",
+          iconName: "011"
+        },
+        {
+          label: "label 12",
+          iconName: "012"
+        }
+      ]
+    };
+  },
+  methods: {
+    setBackground: function(event) {
+      this.isStarryNight = false;
+      this.isYinYang = false;
+      this.isPolkaDot = false;
+      this.isCicadaStripes = false;
+
+      switch (event.target.classList[1]) {
+        case "starry-night":
+          this.isStarryNight = true;
+          break;
+        case "yin-yang":
+          this.isYinYang = true;
+          break;
+        case "polka-dot":
+          this.isPolkaDot = true;
+          break;
+        case "cicada-stripes":
+          this.isCicadaStripes = true;
+          break;
+      }
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+
+  div.draggable {
+    display: flex;
+    flex-wrap: wrap;
+
+    div.background-selector {
+      div.icon-styles {
+        border-radius: 50%;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/IconWrapper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="icon-wrapper">
+    <div class="icon-styles" v-bind:style="{
+    'background-image': 'url(' + iconUrl + ')'}"></div>
+    <p>{{icon.label}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IconWrapper",
+  props: {
+    icon: Object,
+    isHighResScreen: Boolean
+  },
+  computed: {
+    iconUrl: function() {
+      if (this.$props.isHighResScreen) {
+        return require(`../assets/${this.$props.icon.iconName}@2x.png`);
+      }
+      return require(`../assets/${this.$props.icon.iconName}.png`);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+div.icon-wrapper {
+  width: 80px;
+  margin-left: calc(25% - 40px);
+  margin-right: calc(25% - 40px);
+  margin-bottom: 16px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  @media (min-width: 500px) {
+    margin-left: calc(12.5% - 40px);
+    margin-right: calc(12.5% - 40px);
+  }
+
+  @media (min-width: 1000px) {
+    margin-left: calc(8.33% - 40px);
+    margin-right: calc(8.33% - 40px);
+  }
+
+  div.icon-styles {
+    background-size: 80px;
+    width: 80px;
+    height: 80px;
+  }
+
+  p {
+    color: white;
+  }
+}
+</style>
